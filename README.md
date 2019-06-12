d6dab988f153ba0ad30ca52196f0c57838e499f3
wzr <470557449@qq.com>
2019-06-12 16:39:41 +0800

    sql文件

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-# 用node连接mysql创建一个CRUD小项目
+# 用 node 连接 mysql 创建一个 CRUD 小项目
 
-* 登录功能
-* CRUD功能
+- 登录功能
+- CRUD 功能
+
+##怎么执行？
+在项目根目路执行 node server 启动服务，然后访问 localhost:8080/admin 即可
---
+++ shiyan.sql
@@ -0,0 +1,50 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : laotie666
+ Source Server Type    : MySQL
+ Source Server Version : 80012
+ Source Host           : localhost:3306
+ Source Schema         : shiyan
+
+ Target Server Type    : MySQL
+ Target Server Version : 80012
+ File Encoding         : 65001
+
+ Date: 05/06/2019 17:02:09
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for admin_table
+-- ----------------------------
+DROP TABLE IF EXISTS `admin_table`;
+CREATE TABLE `admin_table` (
+  `ID` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `password` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  PRIMARY KEY (`ID`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
+
+-- ----------------------------
+-- Records of admin_table
+-- ----------------------------
+BEGIN;
+INSERT INTO `admin_table` VALUES (1, 'wangzirui', '123456');
+COMMIT;
+
+-- ----------------------------
+-- Table structure for banner_table
+-- ----------------------------
+DROP TABLE IF EXISTS `banner_table`;
+CREATE TABLE `banner_table` (
+  `ID` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '大标题',
+  `description` varchar(300) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '描述文字',
+  `href` varchar(300) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '点击连接',
+  PRIMARY KEY (`ID`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
+
+SET FOREIGN_KEY_CHECKS = 1;
